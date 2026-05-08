3824c275e4a8f5ed1bee7ffc2fcc1e67b20d9587
Khoda <Kazi.Khoda@sdsmt.edu>
2026-05-07 23:32:37 -0600

    Add Impact verification meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta
+          name="impact-site-verification"
+          value="203afec9-16dc-4c42-859b-402fc403f74c"
+        />
+      </head>
+
       <body>{children}</body>
     </html>
   );
